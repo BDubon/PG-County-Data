2fcfce3347303745166b39cb7ea0b5ca63fa3b89
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-17 19:34:20 -0500

    fist commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(1337, fucntion() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Your app is running at http://%s:s%', host, port)
+});
+
