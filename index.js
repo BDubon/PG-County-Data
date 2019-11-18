692fa0f5a826f34d309811ea2ebaa817d5e664ee
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-18 00:57:43 -0500

    Added API call and rendering

--- index.js
+++ index.js
@@ -1,53 +1,41 @@
 const express = require('express');
 const app = express();
+const request = require('request')
 
 
-
-// Application settings
+// *** APP SETTINGS ***
 app.use(express.static('public')); // Automatically directs app to `public` folder to access static files
 app.set('view engine', 'pug');    // View rendering engine
 
 
-// Add different routes here
+// *** ADD THE ROUTES HERE ***
 // Home page
 app.get('/', (req, res) => {
-    let articles = [
-        {
-            id: 1,
-            title: 'John Doe',
-            author: 'john@gmail.com',
-            body: 'active'
-        },
-        {
-            id: 2,
-            title: 'Bob Williams',
-            author: 'bob@gmail.com',
-            body: 'inactive'
-        },
-        {
-            id: 3,
-            title: 'Shannon Jackson',
-            author: 'shannon@gmail.com',
-            body: 'active'
-        }
-    ];
+    const URL = 'https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json'; 
 
-    res.render('index', {
-        title: 'My Title', 
-        message:'My Other Random Message',
-        articles: articles
-    });
-})
+    // Make APi call 
+    request.get(URL, (err, response, body) => {
+        if (err) {
+            return next(err);
+        };
+
+        // Pass data to the view
+        res.render('index', {
+            title: 'My Title', 
+            message:'My Other Random Message',
+            data : JSON.parse(body)
+        });
+    });    
+});
 
 app.get('/api/members', (req, res) => res.json(members));
 
 // Other page
 
 
-// Server setup
+// *** SERVER SETUP ***
 const PORT = process.nextTick.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// Access app at localhost:3000 after running "node index.js" in the terminal
 
