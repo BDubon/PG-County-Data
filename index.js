053c69d459104d3b57259a413ddf9cf8ba24748a
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-17 19:38:47 -0500

    Fixed typo

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-var server = app.listen(1337, fucntion() {
-    var host = server.address().address;
+app.get('/', function(req, res){
+    res.send('Hello World!');
+})
+
+var server = app.listen(1337, function() {
+    // var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Your app is running at http://%s:s%', host, port)
+    console.log('Your app is running at http://localhost:%s', port)
 });
 
